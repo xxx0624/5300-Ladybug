5888c2b92c96d1bddab9c0bbb13cd5de3266627e
xxx0624 <xingzheng.cs@gmail.com>
2020-04-11 14:58:13 -0700

    add more parameter opts into command line

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
+#CFLAGS = -std=c++11 -lstdc++ -Wall -I/usr/local/db6/include -L/usr/local/db6/lib
+#all:
+#	$(CXX) $(CFLAGS) sqlShell.cpp -o sqlshell -lsqlparser
 
-CFLAGS = -std=c++11 -lstdc++ -Wall -I/usr/local/db6/include -L/usr/local/db6/lib
+sqlshell: sqlShell.o
+	g++ -L/usr/local/db6/lib -o $@ $< -ldb_cxx -lsqlparser
 
-all:
-	$(CXX) $(CFLAGS) sqlShell.cpp -o sqlshell -lsqlparser
-
+sqlShell.o : sqlShell.cpp
+	g++ -I/usr/local/db6/include -DHAVE_CXX_STDHEADERS -D_GNU_SOURCE -D_REENTRANT -O3 -std=c++11 -c -o $@ $<
--- sqlShell.cpp
+++ sqlShell.cpp
@@ -5,6 +5,7 @@
 
 #include "SQLParser.h"
 #include "sqlhelper.h"
+#include "db_cxx.h"
 
 using namespace std;
 
@@ -17,24 +18,37 @@ string stringToUpper(string oString){
 }
 
 int main(int argc, char *argv[]) {
-    hsql::SQLParserResult *result;
+    if(argc <= 1){
+        fprintf(stderr, "Usage: ./sqlshell cpsc5300/data\n");
+        return -1;
+    }
+    const char *home = getenv("HOME");
+	string envdir = string(home) + "/" + argv[1];
+    cout << "(sqlshell: running with database environment at " + envdir + ")" << endl;
+
+    DbEnv env(0U);
+	env.set_message_stream(&cout);
+	env.set_error_stream(&cerr);
+	env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
+
     while(true) {
         cout << "SQL>";
         string query;
         getline(cin, query);
         if(stringToUpper(query) == "QUIT"){
             cout << "quit" << endl;
-            delete result;
+            env.close(0U);
             return 0;
         }
         // parse the given query
-        result = hsql::SQLParser::parseSQLString(query);
+        hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(query);
         if (!result->isValid()) {
             cout << "Invalid SQL: " << query << endl;
-            continue;
+        } else {
+            for (uint i = 0; i < result->size(); ++i) {
+                hsql::printStatementInfo(result->getStatement(i));
+            }
         }
-        for (uint i = 0; i < result->size(); ++i) {
-            hsql::printStatementInfo(result->getStatement(i));
-        }
+        delete result;
     }
 }
