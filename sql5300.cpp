69a7f59ff84dbd8edf8c309c15ec49b4ee463e5c
Ethan Guttman <Ethanguttman@gmail.com>
2020-04-18 15:44:57 -0700

    Comments for sql5300.cpp

--- sql5300.cpp
+++ sql5300.cpp
@@ -1,5 +1,7 @@
 /*
  * @file sql5300.cpp main entry for relational db
+ * @author Xing Zheng 
+ * @author Ethan Guttman
  */
 
 #include <stdlib.h>
@@ -13,6 +15,10 @@
 #include "mySQLParser.h"
 #include "mySQLParser.cpp"
 
+/*
+ *Function to transform all characters in string to uppercase
+ *@param oString the string to transform
+ */
 std::string stringToUpper(std::string oString){
     for(int i = 0; i < oString.length(); i++){
         oString[i] = toupper(oString[i]);
@@ -20,8 +26,13 @@ std::string stringToUpper(std::string oString){
     return oString;
 }
 
+/**
+ *Main entry to sql5300 program
+ *@args dbenvpath the path to the BerkeleyDB database environment
+ */
 int main(int argc, char *argv[]) {
-    if(argc <= 1){
+    //check for if the path argument exists and fail if it doesn't
+	if(argc <= 1){
         std::cerr << "Usage: ./sql5300 cpsc5300/data" << std::endl;
         return EXIT_FAILURE;
     }
@@ -29,6 +40,8 @@ int main(int argc, char *argv[]) {
 	std::string envdir = std::string(home) + "/" + argv[1];
     std::cout << "(sqlshell: running with database environment at " + envdir + ")" << std::endl;
 
+	//use the path argument to open up the DB environment
+	//if it isn't already open
     DbEnv env(0U);
 	env.set_message_stream(&std::cout);
 	env.set_error_stream(&std::cerr);
@@ -39,6 +52,8 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+	//main body of program that takes input and returns
+	//SQL parsed text back if the input is an SQL command 
     while(true) {
         std::cout << "SQL> ";
         std::string query;
@@ -46,17 +61,23 @@ int main(int argc, char *argv[]) {
         if(query.length() == 0){
             continue;
         }
+		//exit command for program is "quit"
         if(stringToUpper(query) == "QUIT"){
             std::cout << "quit" << std::endl;
             break;
         }
-        // parse the given query
+        // parse the given query, if invalid stop and if valid translate
         hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(query);
         if (!result->isValid()) {
             std::cout << "Invalid SQL: " << query << std::endl;
         } else {
+			//loop to handle cases where there was more than one SQL statement
+			//in the input text
             for (uint i = 0; i < result->size(); ++i) {
                 //hsql::printStatementInfo(result->getStatement(i));
+				
+				//use sqlStatementToString to transform parse result
+				//into something readable and print it out
                 std::cout << myhsql::sqlStatementToString(result->getStatement(i)) << std::endl;
                 //std::cout << "Valid SQL" << std::endl;
             }
