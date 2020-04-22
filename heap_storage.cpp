a4822e16cc9d2eee26a6eb725953831f533c0caf
xxx0624 <xingzheng.cs@gmail.com>
2020-04-21 22:57:59 -0700

    add basic op for slottedPage

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -1,3 +1,8 @@
 #include "heap_storage.h"
-bool test_heap_storage() {return true;}
+
+
+bool test_heap_storage() {
+    std::cout << "hello test" << std::endl;
+    return true;
+}
 /* FIXME FIXME FIXME */
